0547f51cad277cc0e67065e2c977ecab9f4d9c31
Erik <lukasink@gmail.com>
2017-10-24 21:14:47 -0700

    another impressive small error

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 var db;
 
 // connect to the database BEFORE starting the application server - makes sense! //
-mongodb.mongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
